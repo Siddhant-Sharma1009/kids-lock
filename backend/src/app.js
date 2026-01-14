f36acb2cae839d16f6090ebde383f76ee66509e0
Siddhant-Sharma1009 <siddhantsharma1009@gmail.com>
2026-01-14 16:26:44 +0530

    fix cors origin mismatch for vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,12 +7,28 @@ import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
+const allowedOrigins = [
+  "https://kids-lock.vercel.app",
+  "https://kids-lock-kidsafes-projects.vercel.app",
+  "https://kids-lock-mkra.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "https://kids-lock.vercel.app", // EXACT frontend URL
+    origin: function (origin, callback) {
+      // allow requests with no origin (Postman, mobile apps)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("CORS not allowed"), false);
+      }
+    },
     credentials: true,
   })
 );
+
 
 // Handle preflight requests
 app.options("*", cors());
